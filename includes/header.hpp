76681220e93c4e9ba10438e89fdeb6ed5419749b
Tadeo Amigorena <tamigorena@moonsurgical.com>
2024-04-09 12:55:06 +0200

    clean init

--- includes/header.hpp
+++ includes/header.hpp
@@ -1,33 +1,24 @@
-#ifndef HEADER_HPP
-# define HEADER_HPP
+#ifndef BASE_HEADER_HPP
+# define BASE_HEADER_HPP
 
+# include <iostream>
+# include <string.h>
+# include <sstream>
+# include <fstream>
+# include <math.h>
+# include <chrono>
+# include <iostream>
+# include <string.h>
+# include <sstream>
+# include <fstream>
+# include <vector>
 
-# include "base_header.hpp"
+# include "glad/glad.h" 
+# include <GL/glut.h>
+# include <GLFW/glfw3.h>
 
-# include "classes/vec3.hpp"
-# include "classes/mat4.hpp"
-# include "classes/mesh.hpp"
-# include "classes/game.hpp"
-
-
-
-#include "stb_image.h"
-
-# define STB_IMAGE_IMPLEMENTATION
-
-
-
-typedef struct s_mat4x4
-{
-	float v[4][4] = { 0 };
-}				t_mat4x4;
-
-
-scop::mesh		ft_parsing(std::string file_name);
-void		MultiMatrixVector(t_vec3 &i, t_vec3 &o, t_mat4x4 &m);
-char* ft_get_file(std::string file_name);
-void		DrawTriangle(t_triangle tri);
-void framebuffer_size_callback(GLFWwindow* window, int width, int height);
-t_vec3 sub_vec(t_vec3& a, t_vec3& b);
+# include "struct.hpp"
+# include "math/vec3.hpp"
+# include "math/mat4.hpp"
 
 #endif
