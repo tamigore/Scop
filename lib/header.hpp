2d9cd1cd47efb5688cfe09bc5a2424498df372f4
Tadeo Amigorena <tamigorena@moonsurgical.com>
2024-04-09 12:24:11 +0200

    start lib

---
+++ lib/header.hpp
@@ -0,0 +1,24 @@
+#ifndef BASE_HEADER_HPP
+# define BASE_HEADER_HPP
+
+# include <iostream>
+# include <string.h>
+# include <sstream>
+# include <fstream>
+# include <math.h>
+# include <chrono>
+# include <iostream>
+# include <string.h>
+# include <sstream>
+# include <fstream>
+# include <vector>
+
+# include "glad/glad.h" 
+# include <GL/glut.h>
+# include <GLFW/glfw3.h>
+
+# include "struct.hpp"
+# include "math/vec3.hpp"
+# include "math/mat4.hpp"
+
+#endif
---
+++ lib/math/mat4.hpp
@@ -0,0 +1,31 @@
+#ifndef __MAT4_HPP__
+# define __MAT4_HPP__
+
+namespace math
+{
+class mat4
+{
+	public:
+		mat4();
+		~mat4();
+		mat4(const mat4 &src);
+		mat4(float diag);
+
+		mat4&	operator=(const mat4 &rhs);
+		float*	operator[](int index);
+		mat4	operator*(const mat4 &rhs);
+		
+		mat4&	translate(float x, float y, float z);
+		mat4&	rotate(float angle, float x, float y, float z);
+		mat4&	perspective(float fov, float aspect, float near, float far);
+
+		void	print_mat();
+		float&	getValue();
+
+	private:
+		float	value[4][4];
+
+};
+}
+
+#endif
